5f0c61c51ccc85331e146cc5a96b22016b6e22db
gael272 <gaelgautier48@gmail.com>
2024-11-27 14:20:57 +0100

    td

--- index-portfolio.php
+++ index-portfolio.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mon Portfolio</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/portfolio.css">
 </head>
 <body>
     <header>
@@ -18,7 +18,6 @@
         </nav>
     </header>
 
-    <!-- Inclure chaque section ici -->
     <?php include 'Accueil.php'; ?>
     <?php include 'Compétences.php'; ?>
     <?php include 'Formation.php'; ?>
