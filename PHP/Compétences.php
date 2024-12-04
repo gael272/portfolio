fb715bc26d06774ea6f421034e4fe6b4ab699159
gael272 <gaelgautier48@gmail.com>
2024-12-04 16:32:51 +0100

    maj portfolio

--- PHP/Compétences.php
+++ PHP/Compétences.php
@@ -3,8 +3,16 @@ $data = yaml_parse_file('DB-YAML\competences.yaml');
 ?>
 
 <section id="competences">
-    <h2><?php echo $data['titre']; ?></h2>
-    <p><?php echo $data['domaines']; ?></p>
-    
 
+    <h2><?php echo $data['titre']; ?></h2>
+<?php     
+ foreach($data['domaines'] AS $domaine){
+    echo $domaine['nom']."<br>";
+    foreach($domaine['competences'] AS $competence){
+        echo $competence['nom']." : ". $competence['niveau']."<br>";
+    
+    }
+
+ }
+ ?>
 </section>
