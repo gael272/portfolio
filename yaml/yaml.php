9f9af3bf1868097cc955e9a838405e4b7a478554
gael272 <gaelgautier48@gmail.com>
2024-12-16 11:36:43 +0100

    maj yaml

--- yaml/yaml.php
+++ yaml/yaml.php
@@ -8,20 +8,17 @@ require_once('vendor/autoload.php');
 use Symfony\Component\Yaml\Yaml;
 use Symfony\Component\Yaml\Exception\ParseException;
 if (!function_exists('yaml_parse_file')) {
-
-function yaml_parse_file($file){
-	try {
-		return Yaml::parseFile($file);
-	} catch (ParseException $exception) {
-		printf('Unable to parse the YAML string: <b>%s</b>', $exception->getMessage());
+	function yaml_parse_file($file){
+		try {
+			return Yaml::parseFile($file);
+		} catch (ParseException $exception) {
+			printf('Unable to parse the YAML string: <b>%s</b>', $exception->getMessage());
+		}
 	}
-}
 }
 function print_array($data){
 	echo "<pre>";
 	print_r($data);
 	echo "</pre>";
 }
-
-
 ?>
